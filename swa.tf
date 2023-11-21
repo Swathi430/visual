435c51192b4fdfbc7a5ab9ec9877cc119f40de65
Swathi430 <137596406+Swathi430@users.noreply.github.com>
2023-11-21 12:10:13 +0530

    Update swa.tf

--- swa.tf
+++ swa.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "eu-north-1"
-    access_key = "ertyuiooooo"
-    secret_key = "fgashdjjfkkf"
+    access_key = "AKIA4YABMCDJWYARX34I"
+    secret_key = "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"
 }
 resource "aws_vpc" "my_1010_vpc" {
     cidr_block = "10.0.0.0/24"

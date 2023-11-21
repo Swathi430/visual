78f0a2c3fcc62f272f5efcb2d52f3c7603d7edba
Swathi430 <bamanagariswathi@gmail.com>
2023-11-21 11:49:55 +0530

    1st commit

--- swa.tf
+++ swa.tf
@@ -1,18 +1,18 @@
 provider "aws" {
     region = "eu-north-1"
-    access_key = "AKIA4YABMCDJRUOEOOM2"
-    secret_key = "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" 
+    access_key = "ertyuiooooo"
+    secret_key = "fgashdjjfkkf"
 }
-resource "aws_vpc" "my_1011_vpc" {
+resource "aws_vpc" "my_1010_vpc" {
     cidr_block = "10.0.0.0/24"
     tags = {
-      Name ="my_1011_vpc"
-    }
+      Name = "my_1010_vpc"
+    } 
 }
-resource "aws_security_group" "my_1011_sg" {
-    name = "my_sg"
-    vpc_id = "my_1011_vpc"
-    ingress {
+resource "aws_security_group" "my_sg" {
+    name = "my_1010_sg"
+    vpc_id = "my_1010_vpc"
+  ingress {
     description      = "TLS from VPC"
     from_port        = 8080
     to_port          = 8080
